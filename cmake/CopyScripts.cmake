12e8fee9d3b02dc5b7aa1c8f112c68fbcadebc3b
Joonatan Kuosa <joonatan.kuosa@.tut.fi>
2011-01-02 20:30:11 +0200

    Updated copying start scripts. Now they set dll paths correctly for both 32-bit and 64-bit and release and debug versions.

---
+++ cmake/CopyScripts.cmake
@@ -0,0 +1,86 @@
+
+## Copy a single script ##
+# script path
+# script name
+# output name
+MACRO( copy_shell_script script_path script_name output_name )
+
+if( WIN32 )
+	set( extension bat )
+else()
+	set( extension sh )
+endif()
+set( input_script ${script_path}/${script_name}.${extension}.in )
+set( script_file ${output_name}.${extension} )
+
+if( CMAKE_CONFIGURATION_TYPES )
+	foreach( e ${CMAKE_CONFIGURATION_TYPES} )
+		set( output ${PROJECT_BINARY_DIR}/${e}/${script_file} )
+		set( BUILD_TYPE ${e} )
+		message( "Copying script from ${input_script} to ${output}" )
+		configure_file( ${input_script} ${output} )
+	endforeach()
+else()
+	set( output ${PROJECT_BINARY_DIR}/${script_file} )
+	configure_file( ${input_script} ${output} )
+endif()
+
+
+ENDMACRO()
+
+## Copy all start scripts macro ##
+MACRO( copy_start_scripts program_name project_path start_suffix )
+message( "Copying start scripts for project = ${PROJECT_NAME}
+	: exe name = ${program_name}" )
+
+# Copy the single Node start script
+set( PROGRAM_NAME ${program_name} )
+set( DEMO_DATA_PATH ${vl_SOURCE_DIR}/data )
+set( PROJ_PATH_RELATIVE ${project_path} )
+# TODO the LIB_DIR should be user configurable
+# TODO we need to add Debug and Release support
+if( WIN32 )
+	if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+		set( LIB_DIR "d:\\libs" )
+	else()
+		set( LIB_DIR "d:\\libs32" )
+	endif()
+endif()
+
+# TODO should also copy the eqc file so that the hostname and port can be set
+# correctly there also.
+
+set( script_path ${DEMO_DATA_PATH}/shell_scripts )
+#set( script_name start )
+#set( output_name start_${start_suffix} )
+copy_shell_script( ${script_path} start start_${start_suffix} )
+
+#if( NOT CMAKE_CONFIGURATION_TYPES )
+#	configure_file( ${DEMO_DATA_PATH}/shell_scripts/start.sh.in
+#		${PROJECT_BINARY_DIR}/start_${start_suffix}.sh )
+#else()
+#	foreach( e ${CMAKE_CONFIGURATION_TYPES} )
+#		set( output ${PROJECT_BINARY_DIR}/${e}/start_${start_suffix}.bat )
+#configure_file( ${DEMO_DATA_PATH}/shell_scripts/start.bat.in
+			#			${output} )
+			#	endforeach()
+			#endif()
+
+# Copy the multi node listening start script
+
+set( HOSTNAME_TO_LISTEN 127.0.0.1 )
+message( "Hostname to listen = ${HOSTNAME_TO_LISTEN}" )
+
+set( LISTENING_PORT 4243 )
+set( EQUALIZER_SERVER_BIN "eqServer" )
+set( EQUALIZER_CONFIG ${DEMO_DATA_PATH}/eqc/2-node.layout.eqc )
+
+
+set( script_path ${DEMO_DATA_PATH}/shell_scripts )
+set( script_name start_multi_node )
+set( output_name start_multi_${start_suffix} )
+#copy_shell_script( ${script_path} ${script_name} ${output_name} )
+copy_shell_script( ${script_path} start_multi_node start_multi_${start_suffix} )
+
+ENDMACRO()
+
