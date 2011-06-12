b3a3d59cdf9c4c69632b2f49db31b015899e334c
Joonatan Kuosa <joonatan.kuosa@.tut.fi>
2011-06-12 20:29:05 +0300

    Added checking for NaN into VRPN tracker, should remove problems with faulty optical tracking.
    --HG--
    branch : 0.2.0

--- vl/math/math.hpp
+++ vl/math/math.hpp
@@ -86,6 +86,9 @@ struct Transform
 		: position(Ogre::Vector3::ZERO), quaternion(rot)
 	{}
 
+	bool isValid(void) const
+	{ return !quaternion.isNaN() && !position.isNaN(); }
+
 	bool isIdentity(void) const;
 
 	vl::Transform &operator*=(Ogre::Matrix4 const &m);
