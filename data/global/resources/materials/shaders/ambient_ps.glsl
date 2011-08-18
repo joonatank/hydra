2d0afc9f8cb26dacf025e7ccb4c05c96a170398f
Joonatan Kuosa <joonatan.kuosa@.tut.fi>
2011-08-18 17:23:54 +0300

    Shaders: Added ambient occlusion to ambient shader.

--- data/global/resources/materials/shaders/ambient_ps.glsl
+++ data/global/resources/materials/shaders/ambient_ps.glsl
@@ -9,13 +9,24 @@ uniform vec4 lightAmbient;
 uniform vec4 surfaceAmbient;
 uniform vec4 surfaceEmissive;
 
+#ifdef AMBIENT_OCCLUSION
+uniform sampler2D aoMap;
+#endif
+
 in vec4 uv;
 
 out vec4 FragmentColour;
  
 void main(void)
 {
-	vec4 colour = surfaceAmbient*lightAmbient + surfaceEmissive;
+#ifdef AMBIENT_OCCLUSION
+	vec4 aoColour = texture2D(aoMap, uv.xy);
+	vec4 ambient = surfaceAmbient*aoColour*lightAmbient;
+#else
+	vec4 ambient = surfaceAmbient*lightAmbient;
+#endif
+	vec4 colour = ambient + surfaceEmissive;
+
 	colour.a = surfaceAmbient.a;
 	FragmentColour = clamp(colour, 0.0, 1.0);
 }
