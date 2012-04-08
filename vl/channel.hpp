d1fa91bba566d6152b0efec586298bf7780ad009
Joonatan Kuosa <joonatan.kuosa@savantsimulators.com>
2012-04-08 12:50:17 +0300

    Moved StereoCamera to it's own header and cpp files.

--- vl/channel.hpp
+++ vl/channel.hpp
@@ -13,68 +13,17 @@
 #ifndef HYDRA_CHANNEL_HPP
 #define HYDRA_CHANNEL_HPP
 
-#include "camera.hpp"
+
 // Necessary for Window config and Wall
 #include "base/envsettings.hpp"
-// Necessary for frustum type used in projection calculation
-#include "math/frustum.hpp"
 
 #include <OGRE/OgreViewport.h>
 #include <OGRE/OgreTexture.h>
 
+#include "stereo_camera.hpp"
+
 namespace vl
 {
-
-/// @todo this should be implemented as an Ogre Movable object
-/// a replacement for the Ogre::Camera class
-/// before rendering it should be updated with 
-///		head matrix
-///		cyclop transformation
-///		ipd
-class StereoCamera
-{
-public :
-	StereoCamera(void);
-
-	~StereoCamera(void);
-
-	void setHead(vl::Transform const &head);
-
-	void setCamera(vl::CameraPtr cam);
-
-	vl::CameraPtr getCamera(void) const
-	{ return _camera; }
-
-	/// @brief updates the Ogre::Camera's projection and view matrices
-	/// @param eye_x the eye x direction used for this rendering
-	/// For stereo rendering this needs to be called twice with different
-	/// eye_x values. Also you need to call Viewport::update also.
-	void update(vl::scalar eye_x);
-
-	/// @todo should this store the ipd value and have separate updateLeft
-	/// and updateRight methods for the left and right eye?
-//	void setIPD(vl::scalar ipd);
-
-	vl::Frustum &getFrustum(void)
-	{ return _frustum; }
-
-	vl::Frustum const &getFrustum(void) const
-	{ return _frustum; }
-
-	void setFrustum(vl::Frustum const &f)
-	{ _frustum = f; }
-
-private :
-	vl::CameraPtr _camera;
-
-	Ogre::Camera *_ogre_camera;
-
-	vl::Transform _head;
-
-	vl::Frustum _frustum;
-
-};	// class StereoCamera
-
 
 class Channel
 {
---
+++ vl/stereo_camera.cpp
@@ -0,0 +1,78 @@
+/**
+ *	Copyright (c) 2012 Savant Simulators
+ *
+ *	@author Joonatan Kuosa <joonatan.kuosa@savantsimulators.com>
+ *	@date 2012-04
+ *	@file stereo_camera.cpp
+ *
+ *	This file is part of Hydra VR game engine.
+ *	Version 0.4
+ *
+ */
+
+// Interface
+#include "stereo_camera.hpp"
+
+/// -------------------------- StereoCamera ----------------------------------
+vl::StereoCamera::StereoCamera(void)
+	: _camera(0)
+	, _ogre_camera(0)
+{}
+
+vl::StereoCamera::~StereoCamera(void)
+{
+}
+
+void
+vl::StereoCamera::setHead(vl::Transform const &head)
+{
+	_head = head;
+}
+
+void
+vl::StereoCamera::setCamera(vl::CameraPtr cam)
+{
+	_camera = cam;
+	if(cam)
+	{
+		assert(_camera->getNative());
+		_ogre_camera = (Ogre::Camera *)_camera->getNative();
+	}
+	else
+	{ _ogre_camera = 0; }
+}
+
+
+void
+vl::StereoCamera::update(vl::scalar eye_x)
+{
+	assert(_camera);
+	assert(_ogre_camera);
+
+	_frustum.setClipping(_camera->getNearClipDistance(), _camera->getFarClipDistance());
+
+	Ogre::Quaternion wallRot = orientation_to_wall(_frustum.getWall());
+
+	Ogre::Vector3 cam_pos = _camera->getPosition();
+	Ogre::Quaternion cam_quat = _camera->getOrientation();
+
+	Ogre::Vector3 eye(eye_x, 0, 0);
+
+	Ogre::Matrix4 projMat = _frustum.getProjectionMatrix(eye_x);
+	_ogre_camera->setCustomProjectionMatrix(true, projMat);
+
+	// Combine eye and camera positions
+	// Needs to be rotated with head for correct stereo
+	// Do not rotate with wall will cause incorrect view for the side walls
+	//
+	// cam*head*eye
+	Ogre::Vector3 eye_d = cam_quat*(_head.quaternion*eye + _head.position) + cam_pos;
+
+	// Combine camera and wall orientation to get the projection on correct wall
+	// Seems like the wallRotation needs to be inverse for this one, otherwise
+	// left and right wall are switched.
+	Ogre::Quaternion eye_orientation = cam_quat*wallRot.Inverse();
+
+	_ogre_camera->setPosition(eye_d);
+	_ogre_camera->setOrientation(eye_orientation);
+}
